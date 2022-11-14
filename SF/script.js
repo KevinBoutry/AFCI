352a9d4521a30f37317154eb141fd209a9d1f68d
bidule <truc>
2022-11-14 17:24:04 +0100

    SF 1411 home

--- SF/script.js
+++ SF/script.js
@@ -1,27 +1,27 @@
 "use strict";
 
 const input = document.querySelector("input")
-const char = document.getElementsByClassName("hidden");
+const char = document.querySelectorAll(".hidden");
 const btn = document.querySelector("#btn");
-var k;
 
-console.log(char);
+input.addEventListener("change", inputTOvar);
 
 function inputTOvar(k)
 {
     console.log(k.target.value);
-    for (let i = 0 ; i < 55 ; i++)
+    for (let i = 0 ; i < 45 ; i++)
     {
         console.log(char[i].id);
-        if(char[i].id = k.target.value)
+        console.log(char[i].id == k.target.value);
+        if(char[i].id == k.target.value)
         {
             char[i].classList.remove("hidden")
+            return       
         }
-    }    
+    }   
 }
-input.addEventListener("change", inputTOvar);
 
-for (let i = 0 ; i < 150 ; i++)
+for (let i = 0 ; i < 45 ; i++)
 {
 btn.addEventListener("click", ()=>char[i].classList.toggle("hidden"));
 }
